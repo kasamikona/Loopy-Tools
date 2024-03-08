dd1ad7f93f87273d1bb4f798c18c09434dc6f4da
kasamikona <kasamikona@gmail.com>
2024-03-08 17:57:38 +0000

    Move header to a separate source file

---
+++ CompilerTemplate/src/header.s
@@ -0,0 +1,17 @@
+    .section .header.pointers
+    .long __clheader_romfirst
+    .long __clheader_romlast
+    .long 0xa5a5a5a5 /* Placeholder checksum */
+    .long 0xffffffff
+    .long __clheader_sramfirst
+    .long __clheader_sramlast
+    .long 0xfffffffe
+
+    ! NOT REQUIRED, you can put anything you want here.
+    ! 100 bytes available. Pad first 4 for nice alignment.
+    ! Example in the format of Wanwan header:
+!    .section .header.copyright
+!    .long 0
+!    .asciz "(C)1995 GAZIO All right reserved."
+!    .asciz "ver 5.51"
+
--- CompilerTemplate/src/startup.s
+++ CompilerTemplate/src/startup.s
@@ -1,21 +1,3 @@
-    .section .header.pointers
-    .long __clheader_romfirst
-    .long __clheader_romlast
-    .long 0xa5a5a5a5 /* Placeholder checksum */
-    .long 0xffffffff
-    .long __clheader_sramfirst
-    .long __clheader_sramlast
-    .long 0xfffffffe
-
-    ! NOT REQUIRED, and you can put anything you want here.
-    ! Can move to a separate .s file keeping the .section line.
-    ! Example in the format of Wanwan header:
-!    .section .header.copyright
-!    .long 0
-!    .asciz "(C)1995 GAZIO All right reserved."
-!    .asciz "ver 5.51"
-
-
     .section .text.startup
     .globl start
 start:
