323eacd80799003f28060d89e7c7ef34c309442e
kasami <kasamikona@gmail.com>
2024-11-21 16:30:21 +0000

    Implement compressor, add tilesheet extractor

--- Wanwan/wanwan_data.py
+++ Wanwan/wanwan_data.py
@@ -1,78 +1,241 @@
 import io
 import struct
 
-ROM_BASE = 0x0E000000
-RESOURCES = 0x0E070000
-MAX_RESOURCE_LENGTH = 65536
+# Compressor & decompressor implementation by Kasami (2024)
+# For modified LZSS used in "Wanwan Aijou Monogatari" by Alfa System (1995)
+# This compressor uses a naive longest-match search so is not fast,
+# but it is only expected to take up to a few KB per run.
 
-def decompress(compressed, max_length):
-	# compressed is bytes or bytearray input, must contain the entire compressed data
-	# max_length is limit for decompressed data size
-	# returns decompressed bytes to proper end, or None if error
-	if max_length <= 0:
-		max_length = 1<<20 # 1MB more than enough for "unlimited"
+def compress(input_data):
+	# input_data is bytes or bytearray input
+	# returns compressed bytes
+	
+	input_size = len(input_data)
+	output_data = bytearray()
+	
+	# Set up convenience for writing flag bits
+	flags_pointer = 0
+	flags_counter = 0
+	def _write_flag_bit(b):
+		nonlocal output_data, flags_pointer, flags_counter
+		if flags_counter&7 == 0:
+			flags_counter = 0
+			flags_pointer = len(output_data)
+			output_data.append(0)
+		if b:
+			output_data[flags_pointer] |= 1 << flags_counter
+		flags_counter += 1
 	
 	input_ptr = 0
-	decompressed = bytearray()
-	bitcounter = 0
-	flagbits = 0
-	copyoffset = 0
-	copycount = 0
-	with io.BytesIO(compressed) as compressed_stream:
+	while input_ptr < input_size:
+		match_len = 0
+		match_offset = 0
+		available_length = min(input_size-input_ptr, 257)
+		available_offset = min(input_ptr, 4096)
+		
+		# Find the longest possible match in valid search window, biased to nearest (rfind)
+		# being careful not to start any further along than previous character
+		if input_ptr > 0 and available_length >= 2:
+			for try_len in range(2, available_length+1):
+				try_match = input_data[input_ptr:input_ptr+try_len]
+				match_index = input_data.rfind(try_match, input_ptr-available_offset, input_ptr+try_len-1)
+				if match_index == -1:
+					break
+				try_offset = input_ptr-match_index
+				
+				# Exclude short&far matches that can't be encoded, but keep searching
+				if try_len > 2 or try_offset <= 64:
+					match_len = try_len
+					match_offset = try_offset
+		
+		# Encode the result with Wanwan's encoding
+		if match_len > 0:
+			input_ptr += match_len
+			# Emit copy codes for the match
+			_write_flag_bit(0)
+			if match_offset <= 64 and match_len >= 2 and match_len <= 5:
+				# Short copy
+				_write_flag_bit(1)
+				out_o = -match_offset+64
+				out_c = match_len-2
+				output_data.append(out_c<<6 | out_o)
+			elif match_offset <= 4096 and match_len >= 3 and match_len <= 257:
+				# Long copy
+				_write_flag_bit(0)
+				out_o = -match_offset+4096
+				out_c = match_len-2
+				if match_len <= 17:
+					# Non-extended
+					output_data.append(out_c<<4 | out_o>>8)
+					output_data.append(out_o&255)
+				else:
+					# Extended
+					output_data.append(0 | out_o>>8)
+					output_data.append(out_o&255)
+					output_data.append(out_c&255)
+			else:
+				# Search should exclude this case
+				assert(False)
+		else:
+			# Emit literal code for this character
+			_write_flag_bit(1)
+			output_data.append(input_data[input_ptr])
+			input_ptr += 1
+	
+	# Emit EOF marker (long-extended copy with minimum length)
+	_write_flag_bit(0)
+	_write_flag_bit(0)
+	output_data.append(0x0F)
+	output_data.append(0xFF)
+	output_data.append(0x00)
+	return output_data
+
+def decompress(input_data, debug=False):
+	# input_data is a bytes or bytearray, must start with contain the entire data
+	# returns uncompressed bytes to proper end, or None if error
+	
+	output_data = bytearray()
+	
+	# Create a stream for conveniently reading one byte at a time
+	with io.BytesIO(input_data) as input_stream:
+		# Set up convenience function for reading flag bits
+		flags_counter = 0
+		flags_buffer = 0
+		def _read_flag_bit():
+			nonlocal input_stream, flags_buffer, flags_counter
+			if flags_counter == 0:
+				flags_buffer = input_stream.read(1)[0]
+			flags_counter = (flags_counter-1) & 7
+			flag = flags_buffer&1
+			flags_buffer >>= 1
+			return flag
+		
+		# Do decompression unless an error occurs
 		try:
 			while True:
 				# Grab another flag bit
 				# 0 = copy match
 				# 1 = literal byte
-				if bitcounter == 0:
-					flagbits = compressed_stream.read(1)[0]
-				bitcounter = (bitcounter-1) & 7
-				flag_literal = flagbits&1
-				flagbits >>= 1
-				
+				flag_literal = _read_flag_bit()
 				if flag_literal:
-					decompressed.append(compressed_stream.read(1)[0])
-					if len(decompressed) >= max_length:
-						print("Decompressed data exceeded max length")
-						return None
+					b = input_stream.read(1)[0]
+					output_data.append(b)
+					if debug:
+						print(f"{b:02X} ", end="")
 				else:
 					# Grab another flag bit
 					# 0 = long copy
 					# 1 = short copy
-					if bitcounter == 0:
-						flagbits = compressed_stream.read(1)[0]
-					bitcounter = (bitcounter-1) & 7
-					flag_shortcopy = flagbits&1
-					flagbits >>= 1
-					
+					flag_shortcopy = _read_flag_bit()
+					copy_offset = 0
+					copy_count = 0
 					if flag_shortcopy:
 						# Short copy
 						# next byte ccoooooo
 						# cc = count 2..5 (encoded 0..3)
 						# oooooo = offset -64..-1 (encoded 0..63)
-						shortcopy = compressed_stream.read(1)[0]
-						copyoffset = (shortcopy&63)-64
-						copycount = (shortcopy>>6)&3
+						shortcopy = input_stream.read(1)[0]
+						copy_offset = (shortcopy&63)-64
+						copy_count = (shortcopy>>6)&3
+						if debug:
+							print(f"<s/{-copy_offset},{copy_count+2}> ", end="")
 					else:
 						# Long copy
 						# next 2 bytes ccccoooo oooooooo
 						# cccc = count 3..17 (encoded 1..15) or extended/EOF (encoded 0)
 						# oooooooooooo = offset -4096..-1 (encoded 0..4095)
-						longcopy = struct.unpack(">H",compressed_stream.read(2))[0]
-						copyoffset = (longcopy&4095)-4096
-						copycount = (longcopy>>12)&15
-						if copycount == 0:
+						longcopy = struct.unpack(">H",input_stream.read(2))[0]
+						copy_offset = (longcopy&4095)-4096
+						copy_count = (longcopy>>12)&15
+						if copy_count == 0:
 							# Extended count or EOF
 							# next byte = count 3..257 (encoded 1..255) or EOF (encoded 0)
-							copycount = compressed_stream.read(1)[0]
-							if copycount == 0:
+							copy_count = input_stream.read(1)[0]
+							if copy_count == 0:
+								if debug:
+									print(f"<eof>")
 								break
-					for i in range(copycount+2):
-						decompressed.append(decompressed[copyoffset:][0])
-					if len(decompressed) >= max_length:
-						print("Decompressed data exceeded max length")
-						return None
+						if debug:
+							print(f"<l/{-copy_offset},{copy_count+2}> ", end="")
+					for i in range(copy_count+2):
+						output_data.append(output_data[copy_offset:][0])
 		except IndexError:
 			print("Decompression error")
 			return None
-	return decompressed
+	return output_data
+
+def run_self_test(benchmark_only=False):
+	test_lengths = [0, -1, 100, 1000, 5000]
+	test_repeats = 10
+	random_range = 10
+	import random
+	rng = random.Random()
+	#rng.seed(1234)
+	def _hexstr(b):
+		return " ".join([f"{x:02X}" for x in b])
+	if not benchmark_only:
+		print("Running round-trip tests")
+		for tl in test_lengths:
+			if tl > 0:
+				print(f"Testing {tl} random bytes with range {random_range}")
+			elif tl == 0:
+				print(f"Testing empty input")
+			else:
+				print(f"Testing 256 unique bytes with random offset")
+			reps = test_repeats if (tl != 0) else 1
+			for tr in range(reps):
+				x = bytes()
+				if tl > 0:
+					x = bytes([rng.randint(0, random_range-1) for k in range(tl)])
+				elif tl < 0:
+					ro = rng.randint(0, 256)
+					x = bytes([(i+ro)&255 for i in range(256)])
+				y = ww_lzss_compress(x)
+				if y == None:
+					print("X:"+_hexstr(x))
+					print("Failed to compress the above data")
+					return
+				z = ww_lzss_decompress(y)
+				if z == None:
+					print("X:"+_hexstr(x))
+					print("Y:"+_hexstr(y))
+					print("Failed to decompress the above data")
+					return
+				if x != z:
+					print("X:"+_hexstr(x))
+					print("Failed to round-trip the above data")
+					return
+				print(f"{len(x)} -> {len(y)} -> {len(z)} PASS")
+		print("All tests passed")
+	benchmark_length = 256*224+2
+	benchmark_repeats = 5
+	import time, math
+	sum_time_compress = 0
+	sum_time_decompress = 0
+	sum_ratio = 0
+	print(f"Benchmarking {benchmark_length} random bytes with range {random_range}")
+	for tr in range(benchmark_repeats):
+		print(f"Run {tr+1}...")
+		x = bytes([rng.randint(0, random_range-1) for k in range(benchmark_length)])
+		ts = time.time()
+		y = ww_lzss_compress(x)
+		time_compress = time.time()-ts
+		ts = time.time()
+		z = ww_lzss_decompress(y)
+		time_decompress = time.time()-ts
+		ratio = len(y)/len(x)
+		sum_time_compress += time_compress
+		sum_time_decompress += time_decompress
+		sum_ratio += ratio
+	comp_ratio = sum_ratio/benchmark_repeats
+	comp_time = sum_time_compress/benchmark_repeats
+	comp_speed = benchmark_length/comp_time
+	decomp_time = sum_time_decompress/benchmark_repeats
+	decomp_speed = benchmark_length/decomp_time
+	print(f"Average compression ratio: {comp_ratio:.02f}")
+	print(f"Average compression time: {comp_time:.03f}s ({round(comp_speed/1024)} KiB/s)")
+	print(f"Average decompression time: {decomp_time:.03f}s ({round(decomp_speed/1024)} KiB/s)")
+
+if __name__ == "__main__":
+	import sys
+	run_self_test(len(sys.argv) > 1)
--- Wanwan/wanwan_resources.py
+++ Wanwan/wanwan_resources.py
@@ -1,11 +1,12 @@
 import sys, os, struct
 from PIL import Image
-import wanwan_data
+from wanwan_data import decompress
 
 ROM_BASE = 0x0E000000
-RESOURCES = 0x0E070000
-MAX_COMPRESSED_LENGTH = 1<<16
-MAX_DECOMPRESSED_LENGTH = 1<<16
+RESOURCES_COUNT_PTR = 0x0E070000
+RESOURCES_TABLE_PTR = 0x0E070004
+MAX_RAW_SIZE = 1<<16
+MAX_DECOMPRESSED_SIZE = 1<<16
 
 def parsenum(x):
 	x = x.lower()
@@ -43,9 +44,78 @@ def check_files(exist, noexist):
 def parse_boolean(s):
 	return s.lower() in ['true', '1', 't', 'y', 'yes']
 
-def cmd_extract(args):
-	if len(args) < 4 or len(args) > 5:
-		print("Usage:", sys.argv[0], sys.argv[1], "<rom.bin> <sprite resource> <palette resource> <output.png> [transparent true/false]")
+def get_resource(rom, index, size=None):
+	rom.seek(RESOURCES_COUNT_PTR - ROM_BASE)
+	num_resources = struct.unpack(">I", rom.read(4))[0]
+	if index < 0 or index >= num_resources:
+		print(f"Resource {index} out of range")
+		return None
+	rom.seek(RESOURCES_TABLE_PTR + index*4 - ROM_BASE)
+	res_ptr = struct.unpack(">I", rom.read(4))[0]
+	if res_ptr < ROM_BASE:
+		print(f"Resource {index} is null")
+		return None
+	if size == None or size <= 0:
+		if index == num_resources-1:
+			print(f"Warning: resource {index} is last entry, reading max size")
+			size = MAX_RAW_SIZE
+		else:
+			smallest_diff = MAX_RAW_SIZE
+			for i in range(num_resources):
+				if i == index:
+					continue
+				rom.seek(RESOURCES_TABLE_PTR + i*4 - ROM_BASE)
+				other_ptr = struct.unpack(">I", rom.read(4))[0]
+				if other_ptr >= res_ptr:
+					diff = other_ptr-res_ptr
+					if smallest_diff > diff:
+						smallest_diff = diff
+			size = smallest_diff
+	rom.seek(res_ptr - ROM_BASE)
+	res_data = rom.read(size)
+	#print(f"Resource {index} size <= {len(res_data)} bytes")
+	return res_data
+
+def cmd_extract(args, cmdline, compressed=False):
+	if len(args) not in [3, 4]:
+		print(f"Usage: {cmdline} <rom.bin> <resource index> <output.bin> [{'compressed size' if compressed else 'size'}]")
+		return
+	
+	path_rom_in = args[0]
+	path_res_out = args[2]
+	if not check_files(exist=[path_rom_in], noexist=[path_res_out]):
+		return
+	
+	res_index = parsenum(args[1])
+	if not res_index:
+		print("Invalid index")
+		return
+	
+	res_size = None
+	if len(args) >= 4:
+		res_size = parsenum(args[3])
+		if not res_size or res_size <= 0:
+			print("Invalid size")
+			return
+	
+	with open(path_rom_in, "rb") as rom:
+		res_data = get_resource(rom, res_index, res_size)
+	
+	if compressed:
+		res_data = decompress(res_data)
+		if not res_data:
+			return
+	
+	with open(path_res_out, "wb") as res:
+		res.write(res_data)
+		print(f"Saved resource {res_index} to {path_res_out}")
+
+def cmd_extract_compressed(args, cmdline):
+	cmd_extract(args, cmdline, True)
+
+def cmd_extract_sprite(args, cmdline):
+	if len(args) not in [4, 5]:
+		print(f"Usage: {cmdline} <rom.bin> <sprite resource> <palette resource> <output.png> [transparent true/(false)]")
 		return
 	
 	path_rom_in = args[0]
@@ -64,55 +134,40 @@ def cmd_extract(args):
 		print("Invalid resource number")
 		return
 	
-	with open(args[0], "rb") as rom:
-		rom.seek(RESOURCES - ROM_BASE)
-		num_resources = struct.unpack(">I", rom.read(4))[0]
-		
-		if res_sprite < 0 or res_sprite >= num_resources:
-			print("Sprite resource out of range")
+	with open(path_rom_in, "rb") as rom:
+		data_sprite = get_resource(rom, res_sprite)
+		data_palette = get_resource(rom, res_palette)
+		if (not data_sprite) or (not data_palette):
 			return
-		if res_palette < 0 or res_palette >= num_resources:
-			print("Palette resource out of range")
-			return
-		
-		rom.seek(RESOURCES+4 + res_sprite*4 - ROM_BASE)
-		res_sprite_ptr = struct.unpack(">I", rom.read(4))[0]
-		rom.seek(RESOURCES+4 + res_palette*4 - ROM_BASE)
-		res_palette_ptr = struct.unpack(">I", rom.read(4))[0]
-		
-		if res_sprite_ptr == 0 or res_palette_ptr == 0:
-			print("Null resource")
-			return
-		
-		rom.seek(res_palette_ptr-ROM_BASE)
-		unk, palette_length = struct.unpack("2B", rom.read(2))
-		palette_data = struct.unpack(f">{palette_length}H", rom.read(palette_length*2))
-		palette_rgba = [0]*palette_length
-		print("Palette:")
-		for i in range(palette_length):
-			crgb = col2rgb(palette_data[i])
-			chex = rgbhex(crgb) if (i > 0 or not use_transp) else "Transp."
-			if i&3 == 3 or i == palette_length-1:
-				print(chex)
-			else:
-				print(chex.ljust(12), end="")
-			crgba = (*crgb, 0 if (i == 0 and use_transp) else 255)
-			palette_rgba[i] = crgba
-		
-		for i in range(palette_length):
-			for j in range(0, i):
-				if palette_data[i] == palette_data[j]:
-					print(f"Warning: color {i} is a duplicate of {j}")
-					break
-		
-		rom.seek(res_sprite_ptr-ROM_BASE)
-		compressed = rom.read(MAX_COMPRESSED_LENGTH)
 	
-	decompressed = wanwan_data.decompress(compressed, MAX_DECOMPRESSED_LENGTH)
+	unk, palette_size = struct.unpack("2B", data_palette[:2])
+	if palette_size > len(data_palette)-2:
+		print("Palette data incomplete")
+		return
+	palette_data = struct.unpack(f">{palette_size}H", data_palette[2:palette_size*2+2])
+	palette_rgba = [0]*palette_size
+	print("Palette:")
+	for i in range(palette_size):
+		crgb = col2rgb(palette_data[i])
+		chex = rgbhex(crgb) if (i > 0 or not use_transp) else "Transp."
+		if i&3 == 3 or i == palette_size-1:
+			print(chex)
+		else:
+			print(chex.ljust(12), end="")
+		crgba = (*crgb, 0 if (i == 0 and use_transp) else 255)
+		palette_rgba[i] = crgba
+	
+	for i in range(palette_size):
+		for j in range(0, i):
+			if palette_data[i] == palette_data[j]:
+				print(f"Warning: color {i} is a duplicate of {j}")
+				break
+	
+	decompressed = decompress(data_sprite)
 	if not decompressed:
 		return
-	
 	print(f"Decompressed {len(decompressed)} bytes")
+	
 	img_width, img_height = decompressed[:2]
 	if img_width == 0:
 		img_width = 256
@@ -129,20 +184,112 @@ def cmd_extract(args):
 	for x in range(img_width):
 		for y in range(img_height):
 			ci = decompressed[y*img_width+x]
-			if ci >= palette_length:
+			if ci >= palette_size:
 				print(f"Invalid color {ci} at {x},{y}")
 				continue
 			pix[x,y] = palette_rgba[ci]
-	print(f"Saving to {args[3]}")
-	img.save(args[3])
+	print(f"Saving to {path_image_out}")
+	img.save(path_image_out)
+
+def cmd_extract_tilesheet(args, cmdline):
+	if len(args) not in [4, 5]:
+		print(f"Usage: {cmdline} <rom.bin> <sheet resource> <palette resource> <output.png> [transparent (true)/false]")
+		return
+	
+	path_rom_in = args[0]
+	path_image_out = args[3]
+	if not check_files(exist=[path_rom_in], noexist=[path_image_out]):
+		return
+	
+	use_transp = True
+	if len(args) >= 5:
+		use_transp = parse_boolean(args[4])
+	print("Transparency: " + ("YES" if use_transp else "NO"))
+	
+	res_sheet = parsenum(args[1])
+	res_palette = parsenum(args[2])
+	if res_sheet == None or res_palette == None:
+		print("Invalid resource number")
+		return
+	
+	with open(path_rom_in, "rb") as rom:
+		data_sheet = get_resource(rom, res_sheet)
+		data_palette = get_resource(rom, res_palette)
+		if (not data_sheet) or (not data_palette):
+			return
+	
+	unk, palette_size = struct.unpack("2B", data_palette[:2])
+	if palette_size > len(data_palette)-2:
+		print("Palette data incomplete")
+		return
+	palette_data = struct.unpack(f">{palette_size}H", data_palette[2:palette_size*2+2])
+	palette_rgba = [0]*palette_size
+	print("Palette:")
+	for i in range(palette_size):
+		crgb = col2rgb(palette_data[i])
+		chex = rgbhex(crgb) if (i > 0 or not use_transp) else "Transp."
+		if i&3 == 3 or i == palette_size-1:
+			print(chex)
+		else:
+			print(chex.ljust(12), end="")
+		crgba = (*crgb, 0 if (i == 0 and use_transp) else 255)
+		palette_rgba[i] = crgba
+	
+	for i in range(palette_size):
+		for j in range(0, i):
+			if palette_data[i] == palette_data[j]:
+				print(f"Warning: color {i} is a duplicate of {j}")
+				break
+	
+	decompressed = decompress(data_sheet)
+	if not decompressed:
+		return
+	print(f"Decompressed {len(decompressed)} bytes")
+	
+	num_tiles = struct.unpack(">H", decompressed[:2])[0]
+	if num_tiles == 0:
+		print("No tiles")
+		return
+	
+	img_width = 64
+	img_height = ((num_tiles+7)//8) * 8
+	print(f"Sheet contains {num_tiles} tiles, output in {img_width}x{img_height} image")
+
+	decompressed = decompressed[2:]
+	if len(decompressed) != num_tiles*32:
+		print("Data size mismatch")
+		return
+	
+	img = Image.new("RGBA", (img_width, img_height), color = (0,0,0,0))
+	pix = img.load()
+	for t in range(num_tiles):
+		for tx in range(8):
+			for ty in range(8):
+				ci = decompressed[t*32 + ty*4 + tx//2]
+				if tx & 1 == 0:
+					ci >>= 4
+				ci &= 15
+				if ci >= palette_size:
+					print(f"Invalid color {ci} at T{t}:{tx},{ty}")
+					continue
+				ix = (t&7)*8 + tx
+				iy = (t//8)*8 + ty
+				pix[ix,iy] = palette_rgba[ci]
+	print(f"Saving to {path_image_out}")
+	img.save(path_image_out)
 
 def main():
-	commands = {"extract":cmd_extract}
+	commands = {
+		"extract": cmd_extract,
+		"extract-compressed": cmd_extract_compressed,
+		"extract-sprite": cmd_extract_sprite,
+		"extract-tilesheet": cmd_extract_tilesheet,
+	}
 	if len(sys.argv) > 1:
 		command = sys.argv[1].lower()
 		args = sys.argv[2:]
 		if command in commands:
-			commands[command](args)
+			commands[command](args, command)
 			return
 	print("Usage:", sys.argv[0], "<"+("|".join(commands.keys()))+"> ...")
 
